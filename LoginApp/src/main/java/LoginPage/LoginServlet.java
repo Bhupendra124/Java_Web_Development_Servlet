9ca378492c333144d6f1b15c6ccd50c0a3e9d72b
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-09-08 21:11:01 +0530

    [Bhupendra]ADD:Login Page

---
+++ LoginApp/src/main/java/LoginPage/LoginServlet.java
@@ -0,0 +1,45 @@
+package LoginPage;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = {"/LoginServlet"},
+        initParams = {
+                @WebInitParam(name="user", value="Bhupendra"),
+                @WebInitParam( name="password", value = "12345")
+        }
+)
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //get user parameter for userId and password
+        String user=req.getParameter("user");
+        String pwd=req.getParameter("pwd");
+        //get servlet config init params
+        String userID= getServletConfig().getInitParameter("user");
+        String password=getServletConfig().getInitParameter("password");
+
+        if ((userID.equals(user)) && password.equals(pwd) ){
+            req.setAttribute("user",user);
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req,resp);
+
+
+        }else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out=resp.getWriter();
+            out.println("<font color=red>Eiter user name or password is wrong.</font>");
+            rd.include(req, resp);
+        }
+
+    }
+}
---
+++ LoginApp/src/main/webapp/LoginSuccess.jsp
@@ -0,0 +1,30 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wadek
+  Date: 08-09-2021
+  Time: 19:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Success</title>
+</head>
+<body>
+<h2>Hi<%=request.getAttribute("user")%>,Login successful.......</h2>
+<a href="login.html">Login Page</a>
+</body>
+</html>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
